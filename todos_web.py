27ae87b29490851f310ff89f3f2a363aa949e7e4
soham <sohamrvd@gmail.com>
2023-03-18 13:30:26 +0000

    first commit

---
+++ todos_web.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+st.title("My Todo App")
+st.subheader("App created by Soham..")
+st.write("This app is used to check your daily tasks from anywhere.")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+
+st.text_input(label="Enter a task you want to do today: ",
+              placeholder="Add a new task",
+              on_change=add_todo, key='new_todo')
+
+
